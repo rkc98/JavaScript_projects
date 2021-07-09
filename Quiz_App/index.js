8ffcb913258301bd860ca1425677cf513f35ee5a
Rahul <rkc.98@outlook.com>
2021-07-09 08:36:09 +0530

    code formatting and added event to button

--- Quiz_App/index.js
+++ Quiz_App/index.js
@@ -1,72 +1,81 @@
 
 
-const quizdata=[
+const quizdata = [
     {
-        question : 'what is the capital of India',
-        a:'rome',
+        question: 'what is the capital of India',
+        a: 'rome',
         b: 'london',
-        c:'kabul',
+        c: 'kabul',
         d: 'delhi',
-        answer:'d'
+        answer: 'd'
 
     },
     {
-        question : 'what is the most used programming language',
-        a:'Java',
+        question: 'what is the most used programming language',
+        a: 'Java',
         b: 'JavaScirpt',
-        c:'Go',
+        c: 'Go',
         d: 'python',
-        answer:'a' 
+        answer: 'a'
     },
     {
-        question : 'What is indian national animal',
-        a:'Cow',
+        question: 'What is indian national animal',
+        a: 'Cow',
         b: 'dog',
-        c:'cat',
+        c: 'cat',
         d: 'tiger',
-        answer:'d'
+        answer: 'd'
     },
     {
-        question : 'What is indian national game',
-        a:'cricket',
+        question: 'What is indian national game',
+        a: 'cricket',
         b: 'batminton',
-        c:'hockey',
+        c: 'hockey',
         d: 'football',
-        answer:'c'
+        answer: 'c'
     },
     {
-        question : 'which is the biggest ocean in the World',
-        a:'Indian Ocean',
+        question: 'which is the biggest ocean in the World',
+        a: 'Indian Ocean',
         b: 'Atlantic Ocean',
-        c:'Pacific Ocean',
+        c: 'Pacific Ocean',
         d: 'none of these',
-        answer:'d'
+        answer: 'd'
     }
 ]
 
-const questiontxt=document.getElementById('questiontext');
-const a_text=document.getElementById('a_text');
-const b_text=document.getElementById('b_text');
-const c_text=document.getElementById('c_text');
-const d_text=document.getElementById('d_text');
-const submit=document.getElementById('submitbutton');
+const questiontxt = document.getElementById('questiontext');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submit = document.getElementById('submitbutton');
 
 
 
-let currentquestion=0;
+let currentquestion = 0;
 loadquiz();
 
-function loadquiz(){
+function loadquiz() {
 
-    const currentquestiondata=quizdata[currentquestion];
-    questiontxt.innerText=currentquestiondata.question ;
-    a_text.innerText=currentquestiondata.a;
-    b_text.innerText=currentquestiondata.b;
-    c_text.innerText=currentquestiondata.c;
-    d_text.innerText=currentquestiondata.d;
+    const currentquestiondata = quizdata[currentquestion];
+    questiontxt.innerText = currentquestiondata.question;
+    a_text.innerText = currentquestiondata.a;
+    b_text.innerText = currentquestiondata.b;
+    c_text.innerText = currentquestiondata.c;
+    d_text.innerText = currentquestiondata.d;
 
-    
-    currentquestion++;
 
 }
 
+submit.addEventListener("click", () => {
+
+    currentquestion++;
+
+    if (currentquestion < quizdata.length) {
+        loadquiz();
+    }
+    else {
+        alert('you have finished the quiz');
+    }
+});
