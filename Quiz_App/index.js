2d4c94085fb95976480a022008f9cf03dbf28e1a
Rahul <rkc.98@outlook.com>
2021-07-09 06:56:24 +0530

    added javascript to fill q and a in Quiz_app

--- Quiz_App/index.js
+++ Quiz_App/index.js
@@ -43,3 +43,30 @@ const quizdata=[
         answer:'d'
     }
 ]
+
+const questiontxt=document.getElementById('questiontext');
+const a_text=document.getElementById('a_text');
+const b_text=document.getElementById('b_text');
+const c_text=document.getElementById('c_text');
+const d_text=document.getElementById('d_text');
+const submit=document.getElementById('submitbutton');
+
+
+
+let currentquestion=0;
+loadquiz();
+
+function loadquiz(){
+
+    const currentquestiondata=quizdata[currentquestion];
+    questiontxt.innerText=currentquestiondata.question ;
+    a_text.innerText=currentquestiondata.a;
+    b_text.innerText=currentquestiondata.b;
+    c_text.innerText=currentquestiondata.c;
+    d_text.innerText=currentquestiondata.d;
+
+    
+    currentquestion++;
+
+}
+
